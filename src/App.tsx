2cc3fb26b0a7844b628c62a315ceaa3f6db195b1
anthonywandera <wanderatony92@gmail.com>
2025-04-06 11:20:51 +0300

    added a reviews section

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import About from "./components/About";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
+import Reviews from "./components/Reviews";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Hero />
       <About />
       <Projects />
+      <Reviews />
     </>
   );
 }
