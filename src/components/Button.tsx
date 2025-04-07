9d42282ea13d6c64aa6d8834cc7aade45de7734f
anthonywandera <wanderatony92@gmail.com>
2025-04-07 09:12:37 +0300

    adjusted queries

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,7 +4,7 @@ const Button: React.FC<{ children: React.ReactNode; className?: string }> = ({
 }) => {
   return (
     <button
-      className={`flex justify-center font-bold items-center border-1 border-[var(--theme-color-accent)] p-2 rounded bg-[var(--theme-color-accent)] text-[var(--theme-color-background)] hover:cursor-pointer ${className}`}
+      className={`flex justify-center font-bold items-center border-1 border-[var(--theme-color-accent)] p-1 rounded bg-[var(--theme-color-accent)] text-[var(--theme-color-background)] hover:cursor-pointer ${className}`}
     >
       {children}
     </button>
