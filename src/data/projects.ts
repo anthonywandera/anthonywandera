f4c9e548afb02d0ecfa98327951d6acece321ab8
anthonywandera <wanderatony92@gmail.com>
2025-04-24 15:02:12 +0300

    separated data and types

---
+++ src/data/projects.ts
@@ -0,0 +1,58 @@
+import project_image_01 from "/src/assets/service-images/service-image-01.jpg";
+import project_image_02 from "/src/assets/service-images/service-image-02.jpg";
+import project_image_03 from "/src/assets/service-images/service-image-03.jpg";
+
+export class Project {
+  id: string;
+  name: string;
+  image: string;
+  description: string;
+  technologies: string[];
+  url?: string;
+  review?: string;
+
+  constructor({
+    name,
+    technologies,
+    image,
+    description,
+    url,
+    review,
+  }: {
+    name: string;
+    image: string;
+    description: string;
+    technologies: string[];
+    url?: string;
+    review?: string;
+  }) {
+    this.name = name;
+    this.description = description;
+    this.technologies = technologies;
+    this.image = image;
+    this.url = url;
+    this.review = review;
+    this.id = name;
+  }
+}
+
+export const projectsList = [
+  new Project({
+    name: "Project 01",
+    image: project_image_01,
+    description: "Project description",
+    technologies: ["html"],
+  }),
+  new Project({
+    name: "Project 02",
+    image: project_image_02,
+    description: "Project description",
+    technologies: ["html"],
+  }),
+  new Project({
+    name: "Project 03",
+    image: project_image_03,
+    description: "Project description",
+    technologies: ["html"],
+  }),
+];
