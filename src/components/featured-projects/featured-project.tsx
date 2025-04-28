2c47f2051ec7e627dd089cc7facf7ceb8118bf07
anthonywandera <wanderatony92@gmail.com>
2025-04-28 10:28:25 +0300

    created a featured projects section

---
+++ src/components/featured-projects/featured-project.tsx
@@ -0,0 +1,3 @@
+export default function FeaturedProject() {
+  return <div>Featured project</div>;
+}
---
+++ src/components/featured-projects/featured-projects.tsx
@@ -0,0 +1,11 @@
+import Heading from "../heading/heading";
+import Section from "../section/section";
+
+export default function FeaturedProjects() {
+  return (
+    <Section id="featured-projects" center>
+      <Heading title1="Best of my creations" title2="Featured Projects" />
+      <div></div>
+    </Section>
+  );
+}
