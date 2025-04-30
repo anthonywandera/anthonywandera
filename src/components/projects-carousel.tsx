8851015f0c7ea177841efa34caf02b1330a85d9e
anthonywandera <wanderatony92@gmail.com>
2025-04-30 12:37:14 +0300

    added a shadcn carousel component

---
+++ src/components/projects-carousel.tsx
@@ -0,0 +1,34 @@
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import { Project } from "@/data/projects";
+import ProjectItem from "./projects/project-item";
+
+export default function ProjectsCarousel({
+  projects,
+}: {
+  projects: Project[];
+}) {
+  return (
+    <Carousel>
+      <CarouselContent>
+        {projects.map((project) => (
+          <CarouselItem key={project.id} className="md:basis-1/2 lg:basis-1/4">
+            <ProjectItem
+              image={project.image}
+              name={project.name}
+              description={project.description}
+              url={project.url}
+            />
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  );
+}
--- src/components/projects/projects.tsx
+++ src/components/projects/projects.tsx
@@ -1,59 +1,19 @@
 import Heading from "../heading/heading";
 import Section from "../section/section";
-import Project from "./project";
-import { FaAngleRight, FaAngleLeft } from "react-icons/fa";
-
 import { projectsList } from "../../data/projects";
-import { useState } from "react";
+import ProjectsCarousel from "../projects-carousel";
 
 const Projects: React.FC = () => {
-  const [currentPage, setCurrentPage] = useState(0);
-  const allProjects = projectsList.slice();
-  const projectsPerPage = 4;
-  const totalPages = Math.ceil(allProjects.length / projectsPerPage);
-  const startIndex = currentPage * projectsPerPage;
-  const endIndex = startIndex + projectsPerPage;
-  const currentProjects = allProjects.slice(startIndex, endIndex);
-
-  function handleNextPage() {
-    if (currentPage < totalPages - 1) {
-      setCurrentPage(currentPage + 1);
-    }
-  }
-
-  function handlePrevPage() {
-    if (currentPage > 0) {
-      setCurrentPage(currentPage - 1);
-    }
-  }
-
   return (
     <Section id="projects" center>
       <Heading title1="More of my work" title2="Projects" />
       <div className="grid grid-cols-1 grid-rows-[auto_1fr] gap-4 bg-[var(--theme-color-background-shade)] p-4 rounded-3xl">
         <menu className="flex gap-16 max-sm:gap-2.5 justify-self-center items-center font-semibold">
-          <button onClick={handlePrevPage}>
-            <FaAngleLeft />
-          </button>
           <button>Web</button>
           <button>UI/UX</button>
           <button>Graphics</button>
-          <button onClick={handleNextPage}>
-            <FaAngleRight />
-          </button>
         </menu>
-
-        <div className="grid grid-cols-4 max-md:grid-cols-2 gap-4">
-          {currentProjects.map((project) => (
-            <Project
-              key={project.id}
-              image={project.image}
-              name={project.name}
-              description={project.description}
-              url={project.url}
-            />
-          ))}
-        </div>
+        <ProjectsCarousel projects={projectsList} />
       </div>
     </Section>
   );
