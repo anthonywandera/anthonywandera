38d207a920141ff2c8645bb88ecf9fd00f98a366
anthonywandera <wanderatony92@gmail.com>
2025-05-21 18:55:42 +0300

    added smooth scroll

--- src/components/navigation-bar.tsx
+++ src/components/navigation-bar.tsx
@@ -1,12 +1,38 @@
+import { useEffect, useRef } from "react";
+
 export default function NavigationBar() {
+  const nav = useRef<HTMLElement | null>(null);
+
+  useEffect(() => {
+    const navElement = nav.current;
+
+    navElement?.addEventListener("click", (event) => {
+      const target = event.target as HTMLElement;
+
+      if (target.tagName === "a") {
+        const href = target.getAttribute("href");
+
+        if (href) {
+          const section = document.querySelector(href);
+
+          if (section) {
+            section.scrollIntoView({ behavior: "smooth" });
+            event.preventDefault();
+          }
+        }
+      }
+    });
+  });
+
   return (
-    <header className="flex justify-between px-10">
+    <header className="flex justify-between px-10 sticky top-0 bg-white">
       <span>aw</span>
-      <nav className="flex gap-4">
-        <a href="#">Link</a>
-        <a href="#">Link</a>
-        <a href="#">Link</a>
-        <a href="#">Link</a>
+      <nav ref={nav} className="flex gap-4">
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+        <a href="#projects">Projects</a>
+        <a href="#reviews">Reviews</a>
+        <a href="#contact">Contact</a>
       </nav>
     </header>
   );
