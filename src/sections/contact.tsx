ac66f86db964d42dcaff4387c221491ff2c8332d
anthonywandera <wanderatony92@gmail.com>
2025-05-21 17:59:50 +0300

    competed general markup

--- src/sections/contact.tsx
+++ src/sections/contact.tsx
@@ -1,38 +1,51 @@
 import Section from "../components/Section";
 
 export default function Contact() {
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const data = Object.fromEntries(formData);
+    console.log(data);
+  }
+
   return (
     <Section id="contact">
       <h2 className="font-bold mb-2">Contact</h2>
-      <form className="flex flex-col gap-5">
+      <form className="flex flex-col gap-5" onSubmit={handleSubmit}>
         <div>
-          <label htmlFor="subject">Subject</label>
           <input
             id="subject"
             type="text"
             name="subject"
+            placeholder="Subject"
             className="border border-gray-300 rounded p-2 w-full"
+            required
           />
         </div>
 
         <div>
-          <label htmlFor="email">Email</label>
           <input
             id="email"
             type="email"
             name="email"
+            placeholder="Email"
             className="border border-gray-300 rounded p-2 w-full"
+            required
           />
         </div>
 
         <div>
-          <label htmlFor="message">Message</label>
           <textarea
             id="message"
+            placeholder="Message"
+            name="message"
             className="resize-none w-full min-h-20 border border-gray-300 rounded p-2"
+            required
           />
         </div>
-        <button type="button">Submit</button>
+        <button className="bg-gray-200 py-1.5 px-8 rounded font-semibold w-fit">
+          Submit
+        </button>
       </form>
     </Section>
   );
