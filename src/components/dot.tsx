5860cb81e169774b9c28d6fd9fdca748318452a8
anthonywandera <wanderatony92@gmail.com>
2025-05-23 11:04:34 +0300

    styled reviews

--- src/components/dot.tsx
+++ src/components/dot.tsx
@@ -1,5 +1,5 @@
 export default function Dot() {
   return (
-    <span className="p-0.5 bg-[var(--primary-foreground)] rounded-full"></span>
+    <span className="p-0.5 w-fit h-fit bg-[var(--primary-foreground)] rounded-full"></span>
   );
 }
--- src/sections/reviews.tsx
+++ src/sections/reviews.tsx
@@ -1,3 +1,4 @@
+import Dot from "../components/dot";
 import Section from "../components/Section";
 import Title from "../components/title";
 import { clientReviewsList } from "../data";
@@ -6,10 +7,24 @@ export default function Reviews() {
   return (
     <Section id="reviews">
       <Title title_1="What they say" title_2="Client Reviews" />
-      <div className="grid grid-cols-3 gap-4">
+      <div className="grid grid-cols-2 gap-x-10 gap-y-4 max-md:grid-cols-1">
         {clientReviewsList.map((review) => (
-          <div key={review.id} className="bg-[var(--primary)] p-2 rounded">
-            <p className="text-xs mb-2">{review.name}</p>
+          <div key={review.id} className="bg-[var(--primary)] p-4 rounded">
+            <h4 className="text-xs mb-2 flex items-center gap-2">
+              <span>{review.name}</span>
+              {review.position && (
+                <>
+                  <Dot />
+                  <span>{review.position}</span>
+                </>
+              )}
+              {review.company && (
+                <>
+                  <Dot />
+                  <span>{review.company}</span>
+                </>
+              )}
+            </h4>
             <p>{review.message}</p>
           </div>
         ))}
