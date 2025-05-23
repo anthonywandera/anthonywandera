d8ea0b323e9ba5005b16151c1bfd376dd8e8291a
anthonywandera <wanderatony92@gmail.com>
2025-05-23 10:23:34 +0300

    added a project-item

---
+++ src/components/project-item.tsx
@@ -0,0 +1,20 @@
+import { Project } from "../types";
+
+export default function ProjectItem({ project }: { project: Project }) {
+  return (
+    <div className="bg-[var(--primary)] p-2 rounded">
+      <h3 className="mb-2 flex gap-2 items-center font-semibold">
+        <span>{project.title}</span>
+        <span className="p-0.5 bg-[var(--primary-foreground)] rounded-full"></span>
+        <span className="bg-[var(--primary-foreground)] text-[var(--primary)] text-xs px-2 py-0.5 rounded-full">
+          {project.category}
+        </span>
+      </h3>
+      <p className="mb-2 text-sm text-[var(--primary-foreground)]">
+        {project.description.length > 150
+          ? project.description.slice(0, 150) + "..."
+          : project.description}
+      </p>
+    </div>
+  );
+}
--- src/sections/other-projects.tsx
+++ src/sections/other-projects.tsx
@@ -1,4 +1,5 @@
 import Grid_3 from "../components/grid-3";
+import ProjectItem from "../components/project-item";
 import Section from "../components/Section";
 import Title from "../components/title";
 import { projectsList } from "../data";
@@ -11,21 +12,7 @@ export default function OtherProjects() {
       <div className="flex flex-col gap-8">
         <Grid_3>
           {projectsList.map((project) => (
-            <div
-              key={project.title}
-              className="bg-[var(--primary)] p-2 rounded"
-            >
-              <h3 className="mb-2 flex gap-2 items-center font-semibold">
-                <span>{project.title}</span>
-                <span className="p-0.5 bg-[var(--primary-foreground)] rounded-full"></span>
-                <span className="bg-[var(--primary-foreground)] text-[var(--primary)] text-xs px-2 py-0.5 rounded-full">
-                  {project.category}
-                </span>
-              </h3>
-              <p className="mb-2 text-sm text-[var(--primary-foreground)]">
-                {project.description}
-              </p>
-            </div>
+            <ProjectItem key={project.id} project={project} />
           ))}
         </Grid_3>
       </div>
