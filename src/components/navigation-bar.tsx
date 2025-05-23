36c73f5db6b522a963e251ddb061aaa65fa8d4ba
anthonywandera <wanderatony92@gmail.com>
2025-05-23 13:42:44 +0300

    fixed overlaps

--- src/components/navigation-bar.tsx
+++ src/components/navigation-bar.tsx
@@ -2,7 +2,7 @@ import NavigationButton from "./navigation-button";
 
 export default function NavigationBar() {
   return (
-    <header className="flex justify-end py-2 px-14 sticky top-0 z-10 bg-transparent backdrop-blur max-sm:px-6">
+    <header className="flex justify-end py-2 px-14 sticky top-0 z-50 max-sm:px-6">
       <menu className="flex gap-4 max-sm:hidden">
         <NavigationButton to="home">Home</NavigationButton>
         <NavigationButton to="about">About</NavigationButton>
--- src/components/title-2.tsx
+++ src/components/title-2.tsx
@@ -1,6 +1,6 @@
 export default function Title_2({ children }: { children: string }) {
   return (
-    <h2 className="text-3xl font-bold mb-6 sticky top-0 z-10 w-fit ">
+    <h2 className="text-3xl font-bold mb-6 sticky top-0 z-10 bg-transparent backdrop-blur">
       {children}
     </h2>
   );
