320a45aca8ebddd76a061634c53c77d4d60f0bab
anthonywandera <wanderatony92@gmail.com>
2025-05-23 09:48:09 +0300

    added some style

--- src/components/title-2.tsx
+++ src/components/title-2.tsx
@@ -1,6 +1,6 @@
 export default function Title_2({ children }: { children: string }) {
   return (
-    <h2 className="text-3xl font-bold mb-6 sticky top-0 z-10 w-fit bg-[var(--background)]">
+    <h2 className="text-3xl font-bold mb-6 sticky top-0 z-10 w-fit ">
       {children}
     </h2>
   );
