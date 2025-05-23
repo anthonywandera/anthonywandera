950f01b7f578f2eb8a19726ec168aa83806912d6
anthonywandera <wanderatony92@gmail.com>
2025-05-23 14:05:49 +0300

    added my image

--- src/data/profile.ts
+++ src/data/profile.ts
@@ -1,7 +1,10 @@
+import profileImage from "../assets/profile-image.webp";
+
 export const profileData = {
   name: "Anthony Wandera",
   bio: "A tech-driven and creative software enthusiast with a passion for web development, 3D design, and user-centered digital experiences. Actively honing skills in frontend technologies and design tools, with a collaborative mindset and strong interpersonal values. Eager to contribute to innovative tech solutions and continue growing in a fast-paced development environment.",
-  image: "https://mighty.tools/mockmind-api/content/human/102.jpg",
+  // image: "https://mighty.tools/mockmind-api/content/human/102.jpg",
+  image: profileImage,
   profession: ["Web Developer", "3D Designer", "UI/UX Designer"],
   age: new Date().getFullYear() - 2005,
   country: "Kenya",
