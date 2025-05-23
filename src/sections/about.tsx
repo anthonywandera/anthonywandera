4b42eabfa6a1e63478dd4a5f6beb0f5ca667f297
anthonywandera <wanderatony92@gmail.com>
2025-05-23 10:10:38 +0300

    remove skills

--- src/sections/about.tsx
+++ src/sections/about.tsx
@@ -4,22 +4,12 @@ import Title from "../components/title";
 import { profileData } from "../data";
 
 export default function About() {
-  const { bio, skills } = profileData;
+  const { bio } = profileData;
   return (
     <Section id="about">
       <Title title_1="Who am I" title_2="About Me" />
       <div className="flex flex-col gap-4">
         <p>{bio}</p>
-        <ul className="flex gap-2 flex-wrap">
-          {skills.map((skill) => (
-            <li
-              key={skill}
-              className="py-0.5 px-3 rounded-full text-[var(--primary-foreground)] bg-[var(--primary)]"
-            >
-              {skill}
-            </li>
-          ))}
-        </ul>
       </div>
     </Section>
   );
