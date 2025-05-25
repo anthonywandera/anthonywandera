074ce51e03cf7978ab476572d5c2af8d30d2d427
anthonywandera <wanderatony92@gmail.com>
2025-05-25 19:46:51 +0300

    removed characters limit

--- src/components/project-item.tsx
+++ src/components/project-item.tsx
@@ -58,11 +58,11 @@ export default function ProjectItem({ project }: { project: Project }) {
         onClose={() => {
           setShowModal(false);
         }}
-        className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-50 bg-transparent"
+        className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-50 bg-transparent w-[60%] max-md:w-[80%]"
       >
-        <div className="p-2 rounded bg-white text-[var(--primary)]">
+        <div className="p-2 rounded bg-white text-[var(--primary)">
           <img src={project.image} alt={project.title} />
-          <div className="flex flex-warp mb-2">
+          <div className="flex flex-warp mb-2 justify-between">
             <h3 className="flex gap-2 items-center font-semibold">
               <span>{project.title}</span>
               <Dot />
@@ -80,11 +80,7 @@ export default function ProjectItem({ project }: { project: Project }) {
               Visit Site &rarr;
             </a>
           </div>
-          <p className="mb-2 text-sm">
-            {project.description.length > 150
-              ? project.description.slice(0, 150) + "..."
-              : project.description}
-          </p>
+          <p className="mb-2 text-sm">{project.description}</p>
           <ul className="flex gap-2 flex-wrap text-xs">
             {project.technologies.map((tech) => (
               <li
