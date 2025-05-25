2f44f5df46ef9301d06dbb793587586c6265b58d
anthonywandera <wanderatony92@gmail.com>
2025-05-25 19:39:00 +0300

    added link

--- src/components/project-item.tsx
+++ src/components/project-item.tsx
@@ -61,23 +61,25 @@ export default function ProjectItem({ project }: { project: Project }) {
         className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-50 bg-transparent"
       >
         <div className="p-2 rounded bg-white text-[var(--primary)]">
-          <img src={project.image} />
-          <h3 className="mb-2 flex gap-2 items-center font-semibold">
-            <span>{project.title}</span>
-            <Dot />
-            <span className="bg-[var(--primary-foreground)] text-[var(--primary)] text-xs px-2 py-0.5 rounded-full">
-              {project.category}
-            </span>
-            {project.ongoing && (
-              <>
-                <Dot />
-                <span className="text-xs text-green-300">Ongoing</span>
-              </>
-            )}
-            <a href={project.link} className="text-blue-800 font-thin ml-auto">
+          <img src={project.image} alt={project.title} />
+          <div className="flex flex-warp mb-2">
+            <h3 className="flex gap-2 items-center font-semibold">
+              <span>{project.title}</span>
+              <Dot />
+              <span className="bg-[var(--primary-foreground)] text-[var(--primary)] text-xs px-2 py-0.5 rounded-full">
+                {project.category}
+              </span>
+              {project.ongoing && (
+                <>
+                  <Dot />
+                  <span className="text-xs text-green-300">Ongoing</span>
+                </>
+              )}
+            </h3>
+            <a href={project.link} className="text-blue-800 font-thin">
               Visit Site &rarr;
             </a>
-          </h3>
+          </div>
           <p className="mb-2 text-sm">
             {project.description.length > 150
               ? project.description.slice(0, 150) + "..."
