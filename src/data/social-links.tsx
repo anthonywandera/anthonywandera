a5dcb49c13a3c1efd92f01dd19a976ad49744ef7
anthonywandera <wanderatony92@gmail.com>
2025-05-26 13:57:02 +0300

    hidden unnecessary links

--- src/data/social-links.tsx
+++ src/data/social-links.tsx
@@ -3,9 +3,9 @@ import { SocialLink } from "../types";
 import {
   FaGithub,
   FaInstagram,
-  FaPinterestP,
-  FaUpwork,
-  FaXTwitter,
+  // FaUpwork,
+  // FaPinterestP,
+  // FaXTwitter,
 } from "react-icons/fa6";
 import { FaLinkedinIn } from "react-icons/fa";
 
@@ -25,19 +25,19 @@ export const socialLinks: SocialLink[] = [
     icon: <FaInstagram />,
     url: "https://www.instagram.com/to.niee",
   },
-  {
-    name: "X",
-    icon: <FaXTwitter />,
-    url: "",
-  },
-  {
-    name: "Pinterest",
-    icon: <FaPinterestP />,
-    url: "https://www.pinterest.com/anthony_wandera",
-  },
-  {
-    name: "Upwork",
-    icon: <FaUpwork />,
-    url: "https://www.upwork.com/freelancers/~0155756560a990066d?mp_source=share",
-  },
+  // {
+  //   name: "X",
+  //   icon: <FaXTwitter />,
+  //   url: "",
+  // },
+  // {
+  //   name: "Pinterest",
+  //   icon: <FaPinterestP />,
+  //   url: "https://www.pinterest.com/anthony_wandera",
+  // },
+  // {
+  //   name: "Upwork",
+  //   icon: <FaUpwork />,
+  //   url: "https://www.upwork.com/freelancers/~0155756560a990066d?mp_source=share",
+  // },
 ];
