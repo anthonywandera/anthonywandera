966cdc0a04116d498a7c916491f66806726a0111
anthonywandera <wanderatony92@gmail.com>
2025-05-26 13:27:15 +0300

    added an observer

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useRef } from "react";
+
 interface PropTypes {
   children: React.ReactNode;
   className?: string;
@@ -5,14 +7,31 @@ interface PropTypes {
   center?: boolean;
 }
 
+const observer = new IntersectionObserver(
+  ([entry]) => {
+    const targetEl = entry.target as HTMLDivElement;
+
+    console.log(targetEl.getBoundingClientRect());
+  },
+  { threshold: 0.1, root: null }
+);
+
 export default function Section({
   children,
   className,
   id,
   center = true,
 }: PropTypes) {
+  const sectionEl = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const section = sectionEl.current as HTMLDivElement;
+    observer.observe(section);
+  });
+
   return (
     <section
+      ref={sectionEl}
       id={id}
       className={`mb-10 ${center ? "mx-14 max-sm:mx-6" : ""} ${className}`}
     >
