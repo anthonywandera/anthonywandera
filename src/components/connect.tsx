781bd36c1f1f70671cb3ea659ac23894c7fb1fc0
anthonywandera <wanderatony92@gmail.com>
2025-05-27 06:32:00 +0300

    open a new page upon clicking social link

--- src/components/connect.tsx
+++ src/components/connect.tsx
@@ -4,7 +4,7 @@ export default function Connect() {
   return (
     <div className="flex flex-wrap gap-6 items-center">
       {socialLinks.map((link) => (
-        <a key={link.name} href={link.url}>
+        <a key={link.name} href={link.url} target="_blank">
           {link.icon}
         </a>
       ))}
