7024160a654cf5a17b295c062cb86ce256c972b1
anthonywandera <wanderatony92@gmail.com>
2025-05-27 06:50:01 +0300

    added partial loading

--- src/sections/reviews.tsx
+++ src/sections/reviews.tsx
@@ -1,14 +1,25 @@
+import { useState } from "react";
 import Dot from "../components/dot";
 import Section from "../components/Section";
 import Title from "../components/title";
 import { clientReviewsList } from "../data";
 
 export default function Reviews() {
+  const [page, setPage] = useState(1);
+  const reviewsPerPage = 5;
+  const totalPages = Math.ceil(clientReviewsList.length / reviewsPerPage);
+
+  function handleLoadMore() {
+    if (page < totalPages) {
+      setPage((prev) => prev + 1);
+    }
+  }
+
   return (
     <Section id="reviews">
       <Title title_1="What they say" title_2="Client Reviews" />
       <div className="grid grid-cols-2 gap-x-10 gap-y-4 max-md:grid-cols-1">
-        {clientReviewsList.map((review) => (
+        {clientReviewsList.slice(0, reviewsPerPage * page).map((review) => (
           <div key={review.id} className="bg-[var(--primary)] p-4 rounded">
             <h4 className="text-xs mb-2 flex items-center gap-2">
               <span>{review.name}</span>
@@ -29,6 +40,14 @@ export default function Reviews() {
           </div>
         ))}
       </div>
+      {totalPages != page && (
+        <button
+          onClick={handleLoadMore}
+          className="bg-[var(--primary)] rounded p-3 w-full mt-4"
+        >
+          Load More
+        </button>
+      )}
     </Section>
   );
 }
