8afdd9520c581813323417c8f95e6598dc72d269
anthonywandera <wanderatony92@gmail.com>
2025-05-27 06:49:51 +0300

    added partial loading

--- src/sections/other-projects.tsx
+++ src/sections/other-projects.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Grid_3 from "../components/grid-3";
 import ProjectItem from "../components/project-item";
 import Section from "../components/Section";
@@ -5,16 +6,34 @@ import Title from "../components/title";
 import { projectsList } from "../data";
 
 export default function OtherProjects() {
+  const [page, setPage] = useState(1);
+  const projectsPerPage = 5;
+  const totalPages = Math.ceil(projectsList.length / projectsPerPage);
+
+  function handleLoadMore() {
+    if (page < totalPages) {
+      setPage((prev) => prev + 1);
+    }
+  }
+
   return (
     <Section>
       <Title title_1="More of my work" title_2="Other Projects" />
 
       <div className="flex flex-col gap-8">
         <Grid_3>
-          {projectsList.map((project) => (
+          {projectsList.slice(0, projectsPerPage * page).map((project) => (
             <ProjectItem key={project.id} project={project} />
           ))}
         </Grid_3>
+        {totalPages != page && (
+          <button
+            onClick={handleLoadMore}
+            className="bg-[var(--primary)] rounded p-3"
+          >
+            Load More
+          </button>
+        )}
       </div>
     </Section>
   );
