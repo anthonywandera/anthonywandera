22bcf86e42a2b8c1ad5d90228089e3206c587dbf
anthonywandera <wanderatony92@gmail.com>
2025-05-29 10:22:57 +0300

    SEO

--- src/components/connect.tsx
+++ src/components/connect.tsx
@@ -4,7 +4,12 @@ export default function Connect() {
   return (
     <div className="flex flex-wrap gap-6 items-center">
       {socialLinks.map((link) => (
-        <a key={link.name} href={link.url} target="_blank">
+        <a
+          key={link.name}
+          aria-label={"Connect through " + link.name}
+          href={link.url}
+          target="_blank"
+        >
           {link.icon}
         </a>
       ))}
