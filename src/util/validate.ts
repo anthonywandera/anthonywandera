e730e605963d0c8a3adf1fdc1b23bde073e3bf60
anthonywandera <wanderatony92@gmail.com>
2025-06-03 19:55:30 +0300

    checking for max characters

--- src/util/validate.ts
+++ src/util/validate.ts
@@ -4,12 +4,12 @@ export function validate(
   value: string,
   validation: Validation
 ): { valid: boolean; value: string; error: string } {
-  // check max val
-  if (validation.max && typeof +value === "number" && +value > validation.max) {
+  // check max length
+  if (validation.maxLength && value.length > validation.maxLength) {
     return {
       value,
       valid: false,
-      error: `Should not exceed ${validation.max}`,
+      error: `Should not exceed ${validation.max} characters`,
     };
   }
   return { value, valid: true, error: "" };
