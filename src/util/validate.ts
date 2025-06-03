0bb930c68784d084db807654f04804a3a946f3f8
anthonywandera <wanderatony92@gmail.com>
2025-06-03 19:54:27 +0300

    checking for max val

--- src/util/validate.ts
+++ src/util/validate.ts
@@ -4,8 +4,13 @@ export function validate(
   value: string,
   validation: Validation
 ): { valid: boolean; value: string; error: string } {
+  // check max val
   if (validation.max && typeof +value === "number" && +value > validation.max) {
-    return { value, valid: false, error: `` };
+    return {
+      value,
+      valid: false,
+      error: `Should not exceed ${validation.max}`,
+    };
   }
   return { value, valid: true, error: "" };
 }
